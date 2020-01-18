b8b463afb795965f4ba87717972d796ac76e9b2c
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-18 18:38:18 -0500

    //NOW I've created an array named expression wherein each number and operator clicked is added to the array. //Once the expression is the array expression, I can do what I need to do to get the math done. //For now I think this is a great start

--- script/main.js
+++ script/main.js
@@ -1,24 +1,59 @@
-
 let $output = document.querySelector('span.output');
+let expression = [];
+let numbers = document.querySelectorAll("button.num");
+// let val = numbers.values(); // this element contains more than 1 DOMs.
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].onclick = function() {
+    $output.append(this.value);
+    expression.push(this.value);
 
-let pushNumber = () => {
-  //what actually makes up a function?
-  window.alert(`You pressed the number ${'click'.$buttons.value}`);
-
-}
-
-let button = document.querySelectorAll("button.num");
-let val = button.values(); // this element contains more than 1 DOMs.
-for (let i = 0; i < button.length; i++) {
-  button[i].onclick = function() {
-    $output.textContent = (this.value);
   };
 }
 
-let op = document.querySelectorAll("button.operator");
-let act = op.values();
-for (let i = 0; i < op.length; i++) {
-  op[i].onclick = function() {
-    $output.textContent = (this.value);
+let operators = document.querySelectorAll("button.operator");
+// let act = operators.values();
+for (let i = 0; i < operators.length; i++) {
+  operators[i].onclick = function() {
+    $output.append(this.value);
+    expression.push(this.value);
+ //this outputs the operator visually in the output section.
+    //I want it to highlight the operator button, and start a math expression
   }
 }
+
+let $clear = document.querySelector("button.clear");
+let blank = ('');
+
+  let clear = () => {
+    $clear.addEventListener.onclick = function() {
+      $output.append(this.value);
+
+    }
+  }
+
+let $equal = document.querySelector('button.equals');
+let equals = () => {
+  $equal.addEventListener.onClick = function() {
+  console.log('expression');
+}
+}
+
+//NOW I've created an array named "expression" wherein each number and operator clicked is added to the array.
+//Once the "expression" is the array expression, I can do what I need to do to get the math done.
+//For now I think this is a great start
+
+
+//I want a function that adds the value of a number button clicked to the output as well as
+//to an expression until a operator button is clicked. Op Button onClick adds the operator to the expression
+//start over with numbers, and upon clicking equals it outputs the result on the screen as well as "return"
+// let n1 = "";
+// let n2 = "";
+// let op1 = "";
+//
+// if ($output !== operators) {
+//   n1 = n1.concat($output);
+// } else if ($output !== operators) {
+//   n2 = n2.concat($output);
+// } else if ($output == operators) {
+//   op1 = op1.concat($output);
+// }
