95bdcc50a96e74fa581e424fb5f4857ed8f7bb4b
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-18 11:56:08 -0500

    I had to try a few things out. First I got this script to console.log the whole element it was targeting, now I have it outputting the value of the target (which is a number in this case) into the output box at the top. Using .textContent = (this.value);

--- script/main.js
+++ script/main.js
@@ -1,9 +1,39 @@
-let $buttons = document.querySelectorAll('button');
+// let $buttons = document.querySelectorAll('button');
+//
+// let pushNumber = (event) => {
+//   console.log('yesss');
+// }
+//
+// for (let i = 0; i <= $buttons.length; i = i++) {
+//   $buttons[i].addEventListener('click', pushNumber);
+// }
 
-let pushNumber = (event) => {
-  console.log(`You pressed ${document.querySelector('span')}`);
+let $numbers = document.querySelectorAll('button.num');
+let $output = document.querySelector('span.output');
+
+let pushNumber = () => {
+  //what actually makes up a function?
+  window.alert(`You pressed the number ${'click'.$buttons.value}`);
+
 }
 
-for(let i = 0; i < $buttons.length; i = i ++) {
-  $buttons[i].addEventListener('click', pushNumber);
+// let val = document.querySelectorAll('button.num[value=""]');
+let el = document.querySelectorAll("button.num");
+let val = el.values(); // this element contains more than 1 DOMs.
+for (let i = 0; i < el.length; i++) {
+  el[i].onclick = function() {
+    console.log(this.value);//I've got this console.logging the element and value ie"<button class="num" value=""></button>"
+    $output.textContent = (this.value);//I want to change the text in the output span to match the value of the button clicked
+  };
 }
+//I need to make this console.log the NUMBER that you click.
+//so how can I reference that specific button with that specific value?
+
+
+// for (i = 0; i < $numbers[i]; i + 1) {
+//   $numbers[0].addEventListener('click', pushNumber);
+// }
+
+// $numbers = pushNumber();
+
+// $numbers.addEventListener(click, event-listener);
