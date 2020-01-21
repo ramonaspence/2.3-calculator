4e9fb0874ab49e34ecfecbc561114c9666fe1472
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-20 22:43:59 -0500

    I'm not sure what's wrong. This code was working, I changed a couple bits, changed them back (or not I guess) and it's broken. No errors, and no response from the program. Will revisit tomorrow morning

--- script/main.js
+++ script/main.js
@@ -1,31 +1,29 @@
 (function() {
   'use strict';
+  //
+  // function myMath(a, b) {
+  // let $equals = document.querySelector('button.equals');
+  // $equals.addEventListener('click', function) => {
+  //   expression.includes()
+  // }
+  let expression = [];
+  let $output = document.querySelector('span.output');
+  let $numbers = document.querySelectorAll('button.num');
 
-  function myMath(a, b) => {
-    let expression = [];
-    let $equals = document.querySelectorAll("button.equals");
-    if 
-    $equals.addEventListener('click', (event) => {
-
+  for (let i = 0; i < $numbers.length; i++) {
+    $numbers[i].addEventListener('click', (event) => {
+      expression.push(event.target.value);
+      $output.textContent = (event.target.value);
     })
-
-
-    let $output = document.querySelector('span.output');
-
-    let $numbers = document.querySelectorAll("button.num");
-    for (let i = 0; i < $numbers.length; i++) {
-      $numbers[i].addEventListener('click', (event) => {
-        console.log(event.target.value);
-        expression.push(event.target.value);
-
-      })
-    }
-    let $operators = document.querySelectorAll("button.operator");
-    for (let i = 0; i < $operators.length; i++) {
-      $operators[i].addEventListener('click', (event) => {
-        console.log(expression);
-        expression.push(event.target.value);
-      })
-    }
+}
+  let $operators = document.querySelectorAll("button.operator");
+  for (let i = 0; i < $operators.length; i++) {
+    $operators[i].addEventListener('click', (event) => {
+      console.log(event.target.value);
+      expression.push(event.target.value);
+    })
   }
-})();
+  // function equal() =
+
+  // }
+});
